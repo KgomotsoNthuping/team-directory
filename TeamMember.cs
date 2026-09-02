27c16ec80c7c5011cf318e5184b07fa01290d4ed
KgomotsoNthuping <kgomotsonthuping22@gmail.com>
2026-09-02 03:10:38 +0200

    Added the model of team member

---
+++ TeamMember.cs
@@ -0,0 +1,9 @@
+namespace TeamDirectory;
+
+public class TeamMember
+{
+    public string Name { get; set; } = "";
+    public string Role { get; set; } = "";
+    public string Department { get; set; } = "";
+    public string Email { get; set; } = "";
+}
